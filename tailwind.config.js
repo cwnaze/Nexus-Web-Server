f1b839bb7255810667578e9976edc4c4f4b1f160
System Administrator <root@Caseys-MacBook-Air.local>
2024-04-09 20:07:27 -0400

    CONFIG: configured tailwind settings

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,16 @@
 export default {
   content: [],
   theme: {
+    colors: {
+      'dark-blue': '#3D5A80',
+      'blue': '#98C1D9',
+      'light-blue': '#E0FBFC',
+      'orange': '#EE6C4D',
+      'navy': '#293241'
+    },
+    fontFamily: {
+      sans: ['Ubunu Mono', 'monoface']
+    },
     extend: {},
   },
   plugins: [],
