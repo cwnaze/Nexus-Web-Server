8c2c2a14fb9caa71f4e77b89941359a1f0c06a67
System Administrator <root@Caseys-MacBook-Air.local>
2024-04-09 20:38:14 -0400

    FEAT: Set up Tailwind CSS

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [],
+  content: ["./src/**/*.{html,js,svelte,ts}"],
   theme: {
     colors: {
-      'dark-blue': '#3D5A80',
-      'blue': '#98C1D9',
-      'light-blue': '#E0FBFC',
+      'blue': {
+        100: '#E0FBFC',
+        200: '#98C1D9',
+        300: '#3D5A80',
+        400: '#293241'
+      },
       'orange': '#EE6C4D',
-      'navy': '#293241'
     },
-    fontFamily: {
-      sans: ['Ubunu Mono', 'monoface']
-    },
-    extend: {},
   },
   plugins: [],
 }
