050f24f3ed7735256093304ee95f26bac93f1f9f
Casey Nazelrod <cwnaze@gmail.com>
2024-04-18 16:53:04 -0400

    FEAT: MYSQL connection function

---
+++ src/lib/server/mysql.ts
@@ -0,0 +1,13 @@
+import mysql from 'mysql2/promise';
+import { env } from '$env/dynamic/private';
+
+export function ConnectDb() {
+  let db = mysql.createConnection({
+    host: env.DB_HOST,
+    user: env.DB_USER,
+    password: env.DB_PASSWORD,
+    database: env.DB_NAME,
+  });
+
+  return db;
+}
