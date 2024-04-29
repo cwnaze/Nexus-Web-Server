7471a602c2584ebbacb1a2ae2799742e74b29ad6
Casey Nazelrod <cwnaze@gmail.com>
2024-04-29 17:00:54 -0400

    FEAT: check if email exists

--- src/routes/register/+page.server.ts
+++ src/routes/register/+page.server.ts
@@ -19,11 +19,14 @@ export const actions: Actions = {
         const password: string = data.get('password')?.toString() ?? '';
         const confirm_password: string = data.get('confirm-password')?.toString() ?? '';
 
+        const db: Connection = await ConnectDb();
+
         //form validation
         if (f_name === '') return fail(400, {l_name, t_name, email, f_name_missing: true});
         if (l_name === '') return fail(400, {f_name, t_name, email, l_name_missing: true});
         if (t_name === '') return fail(400, {f_name, l_name, email, t_name_missing: true});
         if (email === '') return fail(400, {f_name, l_name, t_name, email_missing: true});
+        console.log(db.query("SELECT email FROM user_info WHERE email = ?", [email]));
         if (email.indexOf('@') === -1 || email.indexOf('.') === -1) return fail(400, {f_name, l_name, t_name, email, email_invalid: true});
         if (password === '') return fail(400, {f_name, l_name, t_name, email, password_missing: true});
         if (password.length < 8) return fail(400, {f_name, l_name, t_name, email, password_invalid: true});
@@ -31,7 +34,6 @@ export const actions: Actions = {
 
         const hash_pass: string = createHash('sha512').update(password).digest('hex');
 
-        const db: Connection = await ConnectDb();
         db.query("INSERT INTO user_info (email, team_name, last_name, first_name, password) VALUES (?, ?, ?, ?, ?)", [email, t_name, l_name, f_name, hash_pass]);
 
         const token: string = jwt.sign({user: {email: email, team_name: t_name, first_name: f_name, last_name: l_name}}, env.JWT_SECRET, {expiresIn: '1h'});
