f9206da8a9229fd8f534a8cade61dbe5800085c3
Casey Nazelrod <cwnaze@gmail.com>
2024-08-06 20:58:03 -0400

    fix: fixed login

--- src/routes/login/+page.server.ts
+++ src/routes/login/+page.server.ts
@@ -1,6 +1,6 @@
 import { ConnectDb } from '$lib/server/mysql';
 import { fail, redirect, type Actions } from '@sveltejs/kit';
-import type { Connection } from 'mysql2/promise';
+import type { Connection, FieldPacket, QueryResult, RowDataPacket } from 'mysql2/promise';
 import { createHash } from 'crypto';
 import jwt from 'jsonwebtoken';
 import { env } from '$env/dynamic/private';
@@ -19,17 +19,18 @@ export const actions: Actions = {
 
         //form validation
         if (email === '') return fail(400, {email_missing: true});
-        const db_email = await db.query("SELECT email FROM user_info WHERE email = ?", [email]);
         if (password === '') return fail(400, {email, password_missing: true});
 
+        const db_email = await db.query("SELECT email FROM user_info WHERE email = ?", [email]);
         if (db_email[0].toString().length === 0) return fail(400, {email, invalid_login: true});
-        const db_pass = await db.query("SELECT password FROM user_info WHERE email = ?", [email]);
+
+        const db_pass: [any, FieldPacket[]] = await db.query("SELECT password FROM user_info WHERE email = ?", [email]);
         const hash_pass: string = createHash('sha512').update(password).digest('hex');
-        if (db_pass[0].toString() !== hash_pass) return fail(400, {email, invalid_login: true});
+        if (db_pass[0][0].password.toString() !== hash_pass) return fail(400, {email, invalid_login: true});
 
         // create JWT token
-        const user = await db.query("SELECT email, team_name, first_name, last_name FROM user_info WHERE email = ?", [email]);
-        const token: string = jwt.sign({user}, env.JWT_SECRET, {expiresIn: '1h'});
+        const user: [any, FieldPacket[]] = await db.query("SELECT email, team_name, first_name, last_name FROM user_info WHERE email = ?", [email]);
+        const token: string = jwt.sign({user: user[0][0]}, env.JWT_SECRET, {expiresIn: '1h'});
         cookies.set('authToken', token, {httpOnly: true, maxAge: 60 * 60, sameSite: 'strict', secure: false, path: '/'});
         throw redirect(302, '/dashboard/challenges');
     }
--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -3,19 +3,19 @@
     export let form;
 </script>
 
-<form class='text-yellow-200 flex flex-col justify-center items-center h-screen text-2xl'>
+<form method='POST' action='?/login' class='text-yellow-200 flex flex-col justify-center items-center h-screen text-2xl'>
     <div class='flex flex-col gap-6 w-128'>
         <div>
             <a href='/' class='underline hover:text-yellow-100'>&larr; Back</a>
         </div>
         <div class='flex flex-col'>
             <label for='email'>Email</label>
-            <input id='email' type='email' placeholder='jdoe@example.com' class='bg-yellow-100 rounded-lg text-blue-100 p-3 placeholder:text-blue-placeholder'>
+            <input id='email' placeholder='jdoe@example.com' name='email' type='text' value={form?.email ?? ''} class='bg-yellow-100 rounded-lg text-blue-100 p-3 placeholder:text-blue-placeholder'>
             {#if form?.email_missing}<p class='text-error'>Email is required</p>{/if}
         </div>
         <div class='flex flex-col'>
             <label for='password'>Password</label>
-            <input id='password' type='password' placeholder='Password' class='bg-yellow-100 rounded-lg text-blue-100 p-3 placeholder:text-blue-placeholder'>
+            <input id='password' placeholder='Password' name='password' type='password' class='bg-yellow-100 rounded-lg text-blue-100 p-3 placeholder:text-blue-placeholder'>
             {#if form?.password_missing}<p class='text-error'>Password is required</p>{/if}
             {#if form?.invalid_login}<p class='text-error'>Email or password is incorrect</p>{/if}
         </div>
