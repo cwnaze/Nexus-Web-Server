613da5612aa4da0fd9c17f3d88316e1bc26ce9a6
Casey Nazelrod <cwnaze@gmail.com>
2024-08-08 16:40:23 -0400

    feat: connect to vm with vnc

--- src/routes/dashboard/challenges/+page.server.ts
+++ src/routes/dashboard/challenges/+page.server.ts
@@ -1,5 +1,9 @@
 import { redirect } from "@sveltejs/kit";
 
+export const ssr = false;
+
 export function load({ locals }) {
     if(!locals.user) return redirect(302, '/login');
 }
+
+
--- src/routes/dashboard/challenges/+page.svelte
+++ src/routes/dashboard/challenges/+page.svelte
@@ -1,7 +1,36 @@
 <script>
-
+    import { onMount } from "svelte";
+    import RFB from "@novnc/novnc/lib/rfb";
     import Navbar from "../navbar.svelte";
-
-</script>
+  
+    let rfb;
+    let container;
+  
+    const connectToVnc = () => {
+      const url = 'ws://172.16.106.132:6080'; // Update with your WebSocket URL
+  
+      rfb = new RFB(container, url, {
+        credentials: { password: "password"},
+      });
+  
+      rfb.addEventListener("connect", () => {
+        console.log("Connected to VNC server");
+      });
+  
+      rfb.addEventListener("disconnect", () => {
+        console.log("Disconnected from VNC server");
+      });
+  
+      rfb.addEventListener("credentialsrequired", () => {
+        console.log("VNC server requires credentials");
+      });
+    };
+  
+    onMount(() => {
+      connectToVnc();
+    });
+  </script>
 
 <Navbar/>
+
+<div id="vnc-container" bind:this={container}></div>
