11d1bb961251ec9f3ae42d14bd9b2e5963d7445a
Casey Nazelrod <cwnaze@gmail.com>
2024-08-09 17:25:33 -0400

    style: added logo to navbar

--- src/routes/dashboard/navbar.svelte
+++ src/routes/dashboard/navbar.svelte
@@ -1,5 +1,6 @@
-<div class='py-6 bg-blue-100 drop-shadow-dark'>
-    <ul class='flex gap-12 text-yellow-200 justify-center text-xl'>
+<div class='p-3 bg-blue-100 drop-shadow-dark flex justify-between items-center'>
+    <img src='/images/NexusLogo.svg' class='' alt='Nexus Logo'>
+    <ul class='flex gap-12 text-yellow-200 justify-center mx-auto text-xl'>
         <li class="hover:underline"><a href="/dashboard/challenges">Challenges</a></li>
         <li>My Team</li>
         <li>Leaderboard</li>
