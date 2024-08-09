a1f8a59ca2e1dc2e701e9b6cf54d5695a995d08d
Casey Nazelrod <cwnaze@gmail.com>
2024-08-09 16:35:44 -0400

    feat: challenge now has link to challenge page

--- src/routes/dashboard/navbar.svelte
+++ src/routes/dashboard/navbar.svelte
@@ -1,6 +1,6 @@
 <div class='py-6 bg-blue-100 drop-shadow-dark'>
     <ul class='flex gap-12 text-yellow-200 justify-center text-xl'>
-        <li>Challenges</li>
+        <li class="hover:underline"><a href="/dashboard/challenges">Challenges</a></li>
         <li>My Team</li>
         <li>Leaderboard</li>
     </ul>
