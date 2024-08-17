33a17063b365f0df93b6b0297823bb1d2d226050
Casey Nazelrod <cwnaze@gmail.com>
2024-08-17 19:52:05 -0400

    feat: redirect to team page if no team

--- src/routes/dashboard/+page.server.ts
+++ src/routes/dashboard/+page.server.ts
@@ -1,5 +1,8 @@
 import { redirect } from "@sveltejs/kit";
 
 export function load({ locals }) {
+    if(!locals.user.user.team_name) {
+        return redirect(302, '/dashboard/team');
+    }
     return redirect(302, '/dashboard/challenges');
 }
