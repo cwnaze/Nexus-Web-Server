d641461c6a6c1a098c87f094023bc21a0467b58b
Casey Nazelrod <cwnaze@gmail.com>
2024-08-17 20:24:09 -0400

    feat: create team and assign to cookie

--- src/routes/dashboard/team/+page.server.ts
+++ src/routes/dashboard/team/+page.server.ts
@@ -1,5 +1,38 @@
-import { redirect } from "@sveltejs/kit";
+import { fail, redirect, type Actions } from '@sveltejs/kit';
+import { ConnectDb } from '$lib/server/mysql';
+import jwt from 'jsonwebtoken';
+import { env } from '$env/dynamic/private';
+import { createHash } from 'crypto';
+import type { Connection, FieldPacket } from 'mysql2/promise';
+
+/** @type {import('./$types').Actions} */
+
+var cookie_info: any;
 
 export async function load({ locals }) {
+    cookie_info = locals.user.user;
     if(!locals.user) return redirect(302, '/login');
+}
+
+export const actions: Actions = {
+    create_team: async ({ cookies, request }) => {
+        const data: FormData = await request.formData();
+        const team_name: string = data.get('team_name')?.toString() ?? '';
+        const password: string = data.get('password')?.toString() ?? '';
+
+        const db: Connection = await ConnectDb();
+
+        if (team_name === '') return fail(400, {team_name, team_name_missing: true});
+        if (password === '') return fail(400, {team_name, password_missing: true});
+        const team_name_check = await db.query("SELECT team_name FROM team_info WHERE team_name = ?", [team_name]);
+        if (team_name_check[0].toString().length > 0) return fail(400, {team_name, team_name_exists: true});
+
+        await db.query("INSERT INTO user_info (team_name) VALUES (?) WHERE email = (?)", [team_name, cookie_info.email]);
+        const hashed_pass = createHash('sha512').update(password).digest('hex');
+        await db.query("INSERT INTO team_info (team_name, password) VALUES (?, ?)", [team_name, hashed_pass]);
+        cookies.delete('authToken', {path: '/'});
+        const token: string = jwt.sign({user: {email: cookie_info.email, team_name: team_name, first_name: cookie_info.first_name, last_name: cookie_info.last_name}}, env.JWT_SECRET, {expiresIn: '1h'});
+        cookies.set('authToken', token, {httpOnly: true, maxAge: 60 * 60, sameSite: 'strict', secure: false, path: '/'});
+        throw redirect(302, '/dashboard/challenges');
+    }  
 }
--- src/routes/dashboard/team/+page.svelte
+++ src/routes/dashboard/team/+page.svelte
@@ -1,19 +1,24 @@
 <script>
     import Navbar from '$lib/components/navbar.svelte';
+
+    export let form;
 </script>
 
 <Navbar/>
 
-<form class='text-yellow-200 flex flex-col justify-center items-center text-xl gap-6 mt-8'>
+<form method='POST' action='?/create_team' class='text-yellow-200 flex flex-col justify-center items-center text-xl gap-6 mt-8'>
     <div class='flex flex-col gap-6 my-5'>
         <h1 class='text-center text-yellow-200 text-4xl px-40'>Create a Team</h1>
         <div class='flex flex-col'>
             <label for='team-name'>Team Name</label>
-            <input id='team-name' placeholder="John Doe's Team" name='team-name' type='text' class='bg-yellow-100 rounded-lg text-blue-100 p-3 placeholder:text-blue-placeholder'/>
+            <input id='team-name' placeholder="John Doe's Team" name='team-name' type='text' value={form?.team_name ?? ''} class='bg-yellow-100 rounded-lg text-blue-100 p-3 placeholder:text-blue-placeholder'/>
+            {#if form?.team_name_missing}<p class='text-error'>Team Name is required</p>{/if}
+            {#if form?.team_name_exists}<p class='text-error'>Team Name already exists</p>{/if}
         </div>
         <div class='flex flex-col'>
             <label for='password'>Team Password</label>
             <input id='password' placeholder='Password' name='password' type='password' class='bg-yellow-100 rounded-lg text-blue-100 p-3 placeholder:text-blue-placeholder'>
+            {#if form?.password_missing}<p class='text-error'>Password is required</p>{/if}
         </div>
         <button class='bg-yellow-200 text-blue-100 font-bold mx-auto p-3 px-16 rounded-lg border-4 border-yellow-200 mt-6 hover:bg-blue-200 hover:text-yellow-200'>Create Team</button>
         <button class='bg-blue-200 text-yellow-200 font-bold mx-auto p-3 px-16 rounded-lg border-4 border-yellow-200 mt-6 hover:bg-yellow-200 hover:text-blue-100'>Join a Team</button>
