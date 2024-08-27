f272d3cb46b305aba850a63841df6c50774954ef
Casey Nazelrod <cwnaze@gmail.com>
2024-08-27 14:22:18 -0400

    style: team points and placement

--- src/routes/dashboard/team/+page.svelte
+++ src/routes/dashboard/team/+page.svelte
@@ -9,4 +9,7 @@
 <Navbar/>
 
 <h1 class='text-4xl font-black text-yellow-200 text-center mt-4'>{data.user_info.team_name}</h1>
-<h2>{data.team_points} Points</h2>
+<div class='flex items-center justify-between mx-40 bg-blue-100 p-5 mt-8'>
+    <h2 class='text-2xl font-bold text-yellow-200'>{data.team_points} Points</h2>
+    <h2 class='text-2xl font-bold text-yellow-200'>#1</h2>
+</div>
