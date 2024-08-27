900df5bdd0f05d09d93b10bce9ef0679c1522d22
Casey Nazelrod <cwnaze@gmail.com>
2024-08-27 15:40:12 -0400

    feat: add team member names to team page

--- src/routes/dashboard/team/+page.server.ts
+++ src/routes/dashboard/team/+page.server.ts
@@ -13,12 +13,19 @@ export async function load({ locals }) {
 
     const db: Connection = await ConnectDb();
     const team_points: any = await db.query("SELECT points FROM solved WHERE team_name = ?", [locals.user.user.team_name]);
+    const team_members: any = await db.query("SELECT first_name, last_name FROM user_info WHERE team_name = ?", [locals.user.user.team_name]);
+
     let total_points = 0;
     if(team_points[0].length != 0) {
         for(let i = 0; i < team_points[0].length; i++) {
             total_points+=team_points[0][i].points;
         }
     }
+    let team_member_names = [];
+    for (let i = 0; i < team_members[0].length; i++) {
+        team_member_names.push(team_members[0][i].first_name + " " + team_members[0][i].last_name);
+        console.log(team_member_names);
+    }
 
-    return { user_info: locals.user.user, team_points: total_points };
+    return { user_info: locals.user.user, team_points: total_points, team_members: team_member_names };
 }
--- src/routes/dashboard/team/+page.svelte
+++ src/routes/dashboard/team/+page.svelte
@@ -3,6 +3,7 @@
     export let data: {
         user_info: { email: string; team_name: string; first_name: string; last_name: string; };
         team_points: { points: number; };
+        team_members: { name: string}[];
     };
 </script>
 
@@ -12,4 +13,17 @@
 <div class='flex items-center justify-between mx-40 bg-blue-100 p-5 mt-8'>
     <h2 class='text-2xl font-bold text-yellow-200'>{data.team_points} Points</h2>
     <h2 class='text-2xl font-bold text-yellow-200'>#1</h2>
+</div>
+<div class='grid grid-cols-3 mx-40 mt-8 gap-8'>
+    <div class='bg-blue-100 p-8 flex flex-col gap-4'>
+        {#each Object.entries(data.team_members) as [index, name]}
+            {#if Number(index) > 0}
+                <hr class='border-blue-200 border-2'/>
+            {/if}
+            <p class='text-lg text-yellow-200 text-center'>{name}</p>
+        {/each}
+    </div>
+    <div class='grid-cols-subgrid col-span-2'>
+        <h3>Point breakdown</h3>
+    </div>
 </div>
