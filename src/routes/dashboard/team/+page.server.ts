bdbbeff2742e0aef90f3e3d50ba511c71d5b1cef
Casey Nazelrod <cwnaze@gmail.com>
2024-08-27 13:26:58 -0400

    feat: add and display team total points

--- src/routes/dashboard/team/+page.server.ts
+++ src/routes/dashboard/team/+page.server.ts
@@ -1,10 +1,24 @@
 import { redirect } from "@sveltejs/kit";
+import { ConnectDb } from '$lib/server/mysql';
+import type { Connection } from 'mysql2/promise';
 
-export function load({ locals }) {
+
+export async function load({ locals }) {
     if(!locals.user) {
         return redirect(302, '/login');
     }
     if(!locals.user.user.team_name) {
         return redirect(302, '/dashboard/team/create')
     }
+
+    const db: Connection = await ConnectDb();
+    const team_points: any = await db.query("SELECT points FROM solved WHERE team_name = ?", [locals.user.user.team_name]);
+    let total_points = 0;
+    if(team_points[0].length != 0) {
+        for(let i = 0; i < team_points[0].length; i++) {
+            total_points+=team_points[0][i].points;
+        }
+    }
+
+    return { user_info: locals.user.user, team_points: total_points };
 }
--- src/routes/dashboard/team/+page.svelte
+++ src/routes/dashboard/team/+page.svelte
@@ -1,7 +1,12 @@
-<script>
+<script lang='ts'>
     import Navbar from '$lib/components/navbar.svelte';
+    export let data: {
+        user_info: { email: string; team_name: string; first_name: string; last_name: string; };
+        team_points: { points: number; };
+    };
 </script>
 
 <Navbar/>
 
-you have a Team
+<h1 class='text-4xl font-black text-yellow-200 text-center mt-4'>{data.user_info.team_name}</h1>
+<h2>{data.team_points} Points</h2>
