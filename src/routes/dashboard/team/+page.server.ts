348a0e9df7cc54d100f2278d04cde47233bee57d
Casey Nazelrod <cwnaze@gmail.com>
2024-10-20 22:44:45 -0400

    chore: get sum of scores in sql query

--- src/routes/dashboard/team/+page.server.ts
+++ src/routes/dashboard/team/+page.server.ts
@@ -11,15 +11,11 @@ export async function load({ locals }) {
     }
 
     const db: Connection = await ConnectDb();
-    const team_points: any = await db.query("SELECT points FROM solved WHERE team_name = ?", [locals.user.user.team_name]);
+    const team_points: any = await db.query("SELECT SUM(points) FROM solved WHERE team_name = ?", [locals.user.user.team_name]);
     const team_members: any = await db.query("SELECT first_name, last_name FROM user_info WHERE team_name = ?", [locals.user.user.team_name]);
 
-    let total_points = 0;
-    if(team_points[0].length != 0) {
-        for(let i = 0; i < team_points[0].length; i++) {
-            total_points+=team_points[0][i].points;
-        }
-    }
+    const total_points = (team_points[0][0]['SUM(points)']);
+
     let team_member_names = [];
     for (let i = 0; i < team_members[0].length; i++) {
         team_member_names.push(team_members[0][i].first_name + " " + team_members[0][i].last_name);
