e703fe50fa3e45a76c6af53c08263a64569e68a3
Casey Nazelrod <cwnaze@gmail.com>
2024-10-30 10:08:00 -0400

    feat: leaderboard

---
+++ src/routes/dashboard/leaderboard/+page.server.ts
@@ -0,0 +1,23 @@
+import { redirect } from "@sveltejs/kit";
+import { ConnectDb } from '$lib/server/mysql';
+import type { Connection } from 'mysql2/promise';
+
+export async function load({ locals }) {
+    if(!locals.user) {
+        return redirect(302, '/login');
+    }
+    if(!locals.user.user.team_name) {
+        return redirect(302, '/dashboard/team/create')
+    }
+
+    const db: Connection = await ConnectDb();
+
+    const leaderboard = await fetch('http://127.0.0.1:8000/leaderboard').then(res => res.json());
+
+    for (let i = 0; i < leaderboard.length; i++) {
+        leaderboard[i][0] = leaderboard[i][0] + " - " + leaderboard[i][1] + " points";
+        leaderboard[i].splice(1, 1);
+    }
+
+    return { user_info: locals.user.user, leaderboard: leaderboard };
+}
--- src/routes/dashboard/leaderboard/+page.svelte
+++ src/routes/dashboard/leaderboard/+page.svelte
@@ -1,5 +1,21 @@
 <script lang='ts'>
     import Navbar from "$lib/components/navbar.svelte";
+
+    export let data: {
+        leaderboard: {team: string}[]
+    };
 </script>
 
 <Navbar />
+
+<h1 class='text-4xl font-black text-yellow-200 text-center mt-16'>Leaderboard</h1>
+<div class='grid grid-cols-3 mx-40 mt-8 gap-8'>
+    {#each data.leaderboard as team, i}
+        <div>
+            <div class='bg-blue-100 p-8 pt-4 flex flex-col gap-4 justify-center row-span-1'>
+                <h2 class='text-3xl text-center font-bold text-yellow-200'>{i + 1}</h2>
+                <h2 class='text-xl text-center font-bold text-yellow-200 text-wrap'>{team}</h2>
+            </div>
+        </div>
+    {/each}
+</div>
